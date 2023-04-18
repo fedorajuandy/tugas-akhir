0178b0eced7ebaa4c85499fb3683fa3bd3a96de1
Fedora Yoshe Juandy <68008883+fedorajuandy@users.noreply.github.com>
2023-04-18 14:44:38 +0000

    modify be

--- app/backend.py
+++ app/backend.py
@@ -1,33 +1,41 @@
-# Client requests to Dalle-Mini Backend server
-
+""" Script to send requests to backend server """
 import base64
 from io import BytesIO
-
 import requests
 from PIL import Image
 
 
+TIMEOUT_VALUE = 10
+
+
 class ServiceError(Exception):
+    """ Class to set status's code """
     def __init__(self, status_code):
         self.status_code = status_code
 
 
 def get_images_from_backend(prompt, backend_url):
-    r = requests.post(backend_url, json={"prompt": prompt})
-    if r.status_code == 200:
-        json = r.json()
+    """ Function to get requested image """
+    request = requests.post(backend_url, json={"prompt": prompt}, timeout=TIMEOUT_VALUE)
+
+    # if success return image and version else error code
+    if request.status_code == 200:
+        json = request.json()
         images = json["images"]
         images = [Image.open(BytesIO(base64.b64decode(img))) for img in images]
         version = json.get("version", "unknown")
         return {"images": images, "version": version}
     else:
-        raise ServiceError(r.status_code)
+        raise ServiceError(request.status_code)
 
 
 def get_model_version(url):
-    r = requests.get(url)
-    if r.status_code == 200:
-        version = r.json()["version"]
+    """ Function to get model's version """
+    request = requests.get(url, timeout=TIMEOUT_VALUE)
+
+    # if success, return model's version else error code
+    if request.status_code == 200:
+        version = request.json()["version"]
         return version
     else:
-        raise ServiceError(r.status_code)
+        raise ServiceError(request.status_code)
