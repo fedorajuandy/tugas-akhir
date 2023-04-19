359899d4de294b2761547d778af82e28483d7535
Fedora Yoshe Juandy <68008883+fedorajuandy@users.noreply.github.com>
2023-04-19 20:01:59 +0000

    add attributes; need ds

--- app/app.py
+++ app/app.py
@@ -9,36 +9,148 @@ backend_url = os.environ["BACKEND_SERVER"] + "/generate"
 
 
 def infer(prompt):
-    """ yatta """
+    """ Send request, generate image, get image """
     response = get_images_from_backend(prompt, backend_url)
     return response["images"]
 
 
+# UI
 with block:
     gr.Markdown("<h1><center>Face Generator</center></h1>")
 
     # Text input only
-    with gr.Group():
-        with gr.Box():
-            with gr.Row().style(mobile_collapse=False, equal_height=True):
+    # with gr.Group():
+        # with gr.Box():
+            # with gr.Row().style(mobile_collapse=False, equal_height=True):
 
-                text = gr.Textbox(
-                    label="Text prompt", max_lines=1
-                ).style(
-                    border=(True, False, True, True),
-                    margin=False,
-                    rounded=(True, False, False, True),
-                    container=False,
-                )
-                btn = gr.Button("Enter").style(
-                    margin=False,
-                    rounded=(False, True, True, False),
-                )
-        gallery = gr.Gallery(label="Result", show_label=False).style(
-            grid=[3], height="auto"
-        )
-        text.submit(infer, inputs=text, outputs=gallery)
-        btn.click(infer, inputs=text, outputs=gallery)
+    # text = gr.Textbox(
+    #     label = "Text prompt",
+    #     max_lines = 1
+    # )
+
+    five_o_clock_shadow = gr.Checkbox(
+        label = "5 o'clock shadow",
+    )
+    arched_eyebrows = gr.Checkbox(
+        label = "Arched eyebrows",
+    )
+    bushy_eyebrows = gr.Checkbox(
+        label = "Bushy eyebrows",
+    )
+    attractive = gr.Checkbox(
+        label = "Attractive",
+    )
+    bags_under_eyes = gr.Checkbox(
+        label = "Bags under eyes",
+    )
+    backend_urlbangs = gr.Checkbox(
+        label = "Backend urlbangs",
+    )
+    big_lips = gr.Checkbox(
+        label = "Big lips",
+    )
+    big_nose = gr.Checkbox(
+        label = "Big nose",
+    )
+    hair_color = gr.Radio(
+        ["black", "blond", "brown", "gray"],
+        label="Hair color"
+    )
+    blurry = gr.Checkbox(
+        label = "Blurry",
+    )
+    cubby = gr.Checkbox(
+        label = "Cubby",
+    )
+    double_chin = gr.Checkbox(
+        label = "Double chin",
+    )
+    eyeblasses = gr.Checkbox(
+        label = "Eyeblasses",
+    )
+    goatee = gr.Checkbox(
+        label = "Goatee",
+    )
+    heavy_makeup = gr.Checkbox(
+        label = "Heavy makeup",
+    )
+    high_cheekbones = gr.Checkbox(
+        label = "High cheekbones",
+    )
+    gender = gr.Radio(
+        ["female", "male"],
+        label="Gender"
+    )
+    mouth_slightly_open = gr.Checkbox(
+        label = "Mouth slightly open",
+    )
+    mustache = gr.Checkbox(
+        label = "Mustache",
+    )
+    narrow_eyes = gr.Checkbox(
+        label = "Narrow eyes",
+    )
+    no_beard = gr.Checkbox(
+        label = "Beard",
+    )
+    oval_face = gr.Checkbox(
+        label = "Oval face",
+    )
+    pale_skin = gr.Checkbox(
+        label = "Pale skin",
+    )
+    pointy_nose = gr.Checkbox(
+        label = "Pointy nose",
+    )
+    receding_hairline = gr.Checkbox(
+        label = "Receding hairline",
+    )
+    rosy_cheeks = gr.Checkbox(
+        label = "Rosy cheeks",
+    )
+    sideburns = gr.Checkbox(
+        label = "Sideburns",
+    )
+    smiling = gr.Checkbox(
+        label = "Smiling",
+    )
+    hair_style = gr.Radio(
+        ["straight", "wavy"],
+        label="Hair color"
+    )
+    wearing_earrings = gr.Checkbox(
+        label = "Wearing earrings",
+    )
+    wearing_hat = gr.Checkbox(
+        label = "Wearing hat",
+    )
+    wearing_lipstick = gr.Checkbox(
+        label = "Wearing lipstict",
+    )
+    wearing_necklace = gr.Checkbox(
+        label = "Wearing necklace",
+    )
+    wearing_necktie = gr.Checkbox(
+        label = "Wearing necktie",
+    )
+    age = gr.Radio(
+        ["old", "young"],
+        label="Age"
+    )
+
+    btn = gr.Button("Run")
+
+    gallery = gr.Gallery(
+        label = "Result image",
+    ).style(
+        grid = [3],
+        height = "auto"
+    )
+
+    text = f"{gender} has {five_o_clock_shadow}"
+
+    text.submit(infer, inputs=text, outputs=gallery)
+    btn.click(infer, inputs=text, outputs=gallery)
 
     gr.Markdown("<p style='text-align: center'>© Fedora Yoshe Juandy</p>")
 
