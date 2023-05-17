4d823f16431134e20524c40241b589ed1afbe85d
Fedora Yoshe Juandy <68008883+fedorajuandy@users.noreply.github.com>
2023-05-18 00:11:31 +0700

    rm useless stuff

--- encode/encode_dataset.py
+++ encode/encode_dataset.py
@@ -1,8 +1,8 @@
-""" Encode a pair of image and text into encoded one """
+""" Encode a pair of image and text into an encoded one """
 
 from pathlib import Path
 from functools import partial
-import os.path
+# import os.path
 import jax
 from flax.jax_utils import replicate
 from flax.training.common_utils import shard
@@ -11,6 +11,7 @@ import webdataset as wds
 import braceexpand
 from vqgan_jax.modeling_flax_vqgan import VQModel
 import pandas as pd
+
 
 DATASETS = "{000..29999}.parquet"
 SHARDS = "/content/drive/MyDrive/Colab Notebooks/dalle-mini/tools/dataset/data_256.tar"
@@ -37,8 +38,6 @@ ds = (
     .batched(TOTAL_BS)
 )
 
-
-
 dl = (
     wds.WebLoader(ds, BATCH_SIZE=None, NUM_WORKERS=2).unbatched().batched(TOTAL_BS)
 )
@@ -46,37 +45,41 @@ dl = (
 vqgan = VQModel.from_pretrained("dalle-mini/vqgan_imagenet_f16_16384")
 vqgan_params = replicate(vqgan.params)
 
+
 @partial(jax.pmap, axis_name="batch")
 def p_encode(batch, params):
-    """ Function to  """
+    """ Function for parallel encoding using pre-trained VQGAN  """
+
     _, indices = vqgan.encode(batch, params=params)
     return indices
 
+
 def encode_dataset(dataloader, output_dir, save_frequency):
-    """ Function to """
+    """ Function to encode all selected data """
+
     images, captions = next(iter(ds))
 
     output_dir.mkdir(parents=True, exist_ok=True)
     all_captions = []
     all_encoding = []
     n_file = 0
-    nfile = 0
-    file_exists = os.path.exists(f"{output_dir}/{nfile}.parquet")
+    # nfile = 0
+    # file_exists = os.path.exists(f"{output_dir}/{nfile}.parquet")
 
     for idx, () in enumerate(tqdm(dataloader)):
         images = images.numpy()
-        n = len(images)
+        # n_image = len(images)
 
-        if n != len(images):
-            print(f"Different sizes {n} vs {len(images)}")
-            images = images[:n]
-            captions = captions[:n]
-            n_file += 1
-            file_exists = os.path.exists(f"{output_dir}/{nfile}.parquet")
+        # if n_image != len(images):
+        #     print(f"Different sizes {n_image} vs {len(images)}")
+        #     images = images[:n_image]
+        #     captions = captions[:n_image]
+        #     n_file += 1
+        #     file_exists = os.path.exists(f"{output_dir}/{nfile}.parquet")
 
-        if not captions or file_exists:
-            print("No images/captions in batch...")
-            continue
+        # if not captions or file_exists:
+        #     print("No images/captions in batch...")
+        #     continue
 
         images = shard(images)
         encoded = p_encode(images, vqgan_params)
@@ -85,7 +88,7 @@ def encode_dataset(dataloader, output_dir, save_frequency):
         all_encoding.extend(encoded.tolist())
 
         if (idx + 1) % save_frequency == 0:
-            print(f"Saving file {n_file}")
+            # print(f"Saving file {n_file}")
             batch_df = pd.DataFrame.from_dict(
                 {"caption": all_captions, "encoding": all_encoding}
             )
@@ -95,10 +98,11 @@ def encode_dataset(dataloader, output_dir, save_frequency):
             n_file += 1
 
     if all_captions:
-        print(f"Saving final file {n_file}")
+        # print(f"Saving final file {n_file}")
         batch_df = pd.DataFrame.from_dict(
             {"caption": all_captions, "encoding": all_encoding}
         )
         batch_df.to_parquet(f"{output_dir}/{n_file:03d}.parquet")
+
 
 encode_dataset(dl, output_dir=encoded_output, save_frequency=SAVE_FREQUENCY)
