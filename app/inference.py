a73a9e06388dc99141e133e9660ace434a6b3677
Fedora Yoshe Juandy <68008883+fedorajuandy@users.noreply.github.com>
2023-08-09 01:21:28 +0700

    model 5esmhe7l-v0

--- app/inference.py
+++ app/inference.py
@@ -15,7 +15,7 @@ from transformers import CLIPProcessor, FlaxCLIPModel
 
 
 # Model to generate image tokens
-MODEL = "fedorajuandy/dalle-mini/model-h80w65c4:v0"
+MODEL = "fedorajuandy/dalle-mini/model-5esmhe7l:v0"
 MODEL_COMMIT_ID = None
 
 # VQGAN to decode image tokens
