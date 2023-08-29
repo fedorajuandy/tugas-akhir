69cf19fcb0fb3ae6e1dd323f8c514dd50903b687
Fedora Yoshe Juandy <68008883+fedorajuandy@users.noreply.github.com>
2023-08-29 14:25:37 +0700

    change prints

--- train/train.py
+++ train/train.py
@@ -897,8 +897,8 @@ def main():
                     **attr_state,
                 )
 
-            print(param_spec)
-            print(state_spec)
+            type(param_spec)
+            type(state_spec)
             state = pjit( # KAEYA
                 init_state,
                 in_axis_resources=(param_spec,)
